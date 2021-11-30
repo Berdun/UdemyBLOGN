a945ccf322c68f29028e1bbdbacbd49c55fb6623
Bahadır ERDUN <bahadirerdun@windowslive.com>
2021-11-30 12:11:16 +0300

    Add project files.

---
+++ BLOGN.API/Program.cs
@@ -0,0 +1,86 @@
+using BLOGN.API;
+using BLOGN.Data;
+using BLOGN.Data.Repositories.IRepository;
+using BLOGN.Data.Repositories.Repository;
+using BLOGN.Data.Services;
+using BLOGN.Data.Services.IService;
+using BLOGN.SharedTools;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors();
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+builder.Services.AddScoped(typeof(IService<>),typeof(Services<>));
+builder.Services.AddScoped<ICategoryService,CategoryService>();
+builder.Services.AddScoped<IArticleService,ArticleService>();
+builder.Services.AddScoped<IUserRepository,UserRepository>();  
+builder.Services.AddScoped<IUserService,UserService>();  
+
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(connectionString)
+);
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+IServiceCollection serviceCollection = builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+builder.Services.AddSwaggerGen();
+
+
+
+var appSettingsSection = builder.Configuration.GetSection("AppSettings");
+
+builder.Services.Configure<AppSettings>(appSettingsSection);
+
+var appSettings = appSettingsSection.Get<AppSettings>();
+var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+
+builder.Services.AddAuthentication(x =>
+{
+     x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+     x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x =>
+{
+     x.RequireHttpsMetadata = false;
+     x.SaveToken = true;
+     x.TokenValidationParameters = new TokenValidationParameters
+     {
+          ValidateIssuerSigningKey = true,
+          IssuerSigningKey = new SymmetricSecurityKey(key),
+          ValidateIssuer = false,
+          ValidateAudience = false
+     };
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+     app.UseSwagger();
+     app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
---
+++ BLOGN.Data/Repositories/IRepository/IArticleRepository.cs
@@ -0,0 +1,14 @@
+﻿using BLOGN.Data.Repositories.Repository;
+using BLOGN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLOGN.Data.Repositories.IRepository
+{
+     public interface IArticleRepository:IRepository<Article>
+     {
+     }
+}

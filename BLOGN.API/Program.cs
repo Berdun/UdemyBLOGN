e57dcf97a241eff88938ff30399aa0bcf05ccd3c
Berdun <52720257+Berdun@users.noreply.github.com>
2021-12-02 12:09:35 +0300

    Update Program.cs

--- BLOGN.API/Program.cs
+++ BLOGN.API/Program.cs
@@ -39,12 +39,6 @@ builder.Services.AddSwaggerGen();
 
 
 
-var appSettingsSection = builder.Configuration.GetSection("AppSettings");
-
-builder.Services.Configure<AppSettings>(appSettingsSection);
-
-var appSettings = appSettingsSection.Get<AppSettings>();
-var key = Encoding.ASCII.GetBytes(appSettings.Secret);
 
 
 builder.Services.AddAuthentication(x =>
